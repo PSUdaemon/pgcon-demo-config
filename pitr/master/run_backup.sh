1100752cb3190d9a94624e5edc4851ee3beaf72e
Phil Sorber <phil@praetorian.omniti.com>
2012-05-10 14:11:05 -0400

    Remove bad option

--- pitr/master/run_backup.sh
+++ pitr/master/run_backup.sh
@@ -3,7 +3,6 @@
     -x /var/lib/pgsql/9.1/backups/xlogs \
     -dl /var/lib/pgsql/9.1/backups \
     -dr failover:/var/lib/pgsql/9.1/backups \
-    -s /var/lib/pgsql/9.1/omnipitr \
     -l /var/lib/pgsql/9.1/demo/pg_log/omnipitr-backup-master-^Y-^m-^d.log \
     -v \
     -dg SHA-1
