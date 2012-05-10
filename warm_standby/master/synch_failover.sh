4581c5cf81bc8e3073ad8ccbd91db0f7c1d7c790
Phil Sorber <phil@praetorian.omniti.com>
2012-05-10 17:08:02 -0400

    Fix log name

--- warm_standby/master/synch_failover.sh
+++ warm_standby/master/synch_failover.sh
@@ -1,5 +1,5 @@
 /opt/omnipitr/bin/omnipitr-synch \
     -D /var/lib/pgsql/9.1/demo \
     -o failover:/var/lib/pgsql/9.1/demo \
-    -l /var/lib/pgsql/9.1/demo/pg_log \
+    -l /var/lib/pgsql/9.1/demo/pg_log/omnipitr-synch-^Y-^m-^d.log \
     -v
