90e1cb07990b153f2f8eb6be3027f0ec931fe3ea
Phil Sorber <phil@praetorian.omniti.com>
2012-05-13 23:40:14 -0400

    add setup scripts

---
+++ streaming_warm_standby/master/setup.sh
@@ -0,0 +1,1 @@
+cp /var/lib/pgsql/9.1/pgcon-demo-config/streaming_warm_standby/maseter/*.conf /var/lib/pgsql/9.1/demo
---
+++ warm_standby/failover/setup.sh
@@ -0,0 +1,1 @@
+cp /var/lib/pgsql/9.1/pgcon-demo-config/warm_standby/failover/*.conf /var/lib/pgsql/9.1/demo
