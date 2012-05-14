59218ca2a2a1f05186058d2147fb4318decf17cf
Phil Sorber <phil@praetorian.omniti.com>
2012-05-14 00:36:18 -0400

    fix path

--- hybrid_streaming_warm_standby/master/setup.sh
+++ hybrid_streaming_warm_standby/master/setup.sh
@@ -1,1 +1,1 @@
-cp /var/lib/pgsql/9.1/pgcon-demo-config/streaming_warm_standby/master/*.conf /var/lib/pgsql/9.1/demo
+cp /var/lib/pgsql/9.1/pgcon-demo-config/hybrid_streaming_warm_standby/master/*.conf /var/lib/pgsql/9.1/demo
