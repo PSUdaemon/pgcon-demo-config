130de4f22fe88256c89a48c5438468b3ee4afc3c
Phil Sorber <phil@praetorian.omniti.com>
2012-05-16 00:13:25 -0400

    setup for 9.2

--- cascading/failover/setup.sh
+++ cascading/failover/setup.sh
@@ -1,1 +1,1 @@
-cp /var/lib/pgsql/9.1/pgcon-demo-config/cascading/failover/*.conf /var/lib/pgsql/9.1/demo
+cp /var/lib/pgsql/9.1/pgcon-demo-config/cascading/failover/*.conf /var/lib/pgsql/9.2/demo92
