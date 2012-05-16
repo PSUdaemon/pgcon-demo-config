9db0b21766ebcd1d3566d41dd2917228f962dbc5
Phil Sorber <phil@praetorian.omniti.com>
2012-05-16 00:02:17 -0400

    fix cascading setup

--- cascading/master/setup.sh
+++ cascading/master/setup.sh
@@ -1,1 +1,1 @@
-cp /var/lib/pgsql/9.1/pgcon-demo-config/hybrid_streaming_hot_standby/master/*.conf /var/lib/pgsql/9.1/demo
+cp /var/lib/pgsql/9.1/pgcon-demo-config/cascading/master/*.conf /var/lib/pgsql/9.1/demo
--- cascading/standby/setup.sh
+++ cascading/standby/setup.sh
@@ -1,1 +1,1 @@
-cp /var/lib/pgsql/9.1/pgcon-demo-config/hybrid_streaming_hot_standby/failover/*.conf /var/lib/pgsql/9.1/demo
+cp /var/lib/pgsql/9.1/pgcon-demo-config/cascading/failover/*.conf /var/lib/pgsql/9.1/demo
