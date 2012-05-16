065e0843e887d37c64d86d993ff76b75c1f01da5
Phil Sorber <phil@praetorian.omniti.com>
2012-05-16 00:55:41 -0400

    misc fixes

--- multi_standby_failover/standby/setup.sh
+++ multi_standby_failover/standby/setup.sh
@@ -1,1 +1,1 @@
-cp /var/lib/pgsql/9.1/pgcon-demo-config/multi_standby_failover/failover/*.conf /var/lib/pgsql/9.1/demo
+cp /var/lib/pgsql/9.1/pgcon-demo-config/multi_standby_failover/standby/*.conf /var/lib/pgsql/9.1/demo
